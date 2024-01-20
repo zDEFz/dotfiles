f8405a51a266b6776651ea6a73f7697c5bc21401
zDEFz <a-development@posteo.de>
2024-01-20 15:17:45 +0100

    initial

---
+++ i3blocks/scripts/bedroom-get-humidity.sh
@@ -0,0 +1,1 @@
+echo "hum. bedroom: $(broadlink_cli --device @/home/blu/broadlink/BEDROOM.device --humid)"
---
+++ i3blocks/scripts/kitchen-get-humidity.sh
@@ -0,0 +1,1 @@
+echo "hum. kitchen: $(broadlink_cli --device @/home/blu/broadlink/KITCHEN.device --humid)"
