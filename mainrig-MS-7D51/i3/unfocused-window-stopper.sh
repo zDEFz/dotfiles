f6ba12a2e9d36a487b673906963602884f38663d
zDEFz <a-development@posteo.de>
2024-04-14 20:00:00 +0200

    Sun Apr 14 08:00:00 PM CEST 2024

--- mainrig-MS-7D51/i3/unfocused-window-stopper.sh
+++ mainrig-MS-7D51/i3/unfocused-window-stopper.sh
@@ -11,7 +11,8 @@ process_stopped=false
 shopt -s extglob
 
 # Get a list of open windows using wmctrl
-open_windows=$(i3-msg -t get_tree | jq -r '.. | .window_properties? | select(.class) | .class')
+open_windows=$(i3-msg -t get_tree | jq -r '.. | .window_properties? | select(.class) | .class'
+)
 
 # Initialize an array to store matching window IDs
 matching_results=()
@@ -27,28 +28,11 @@ for entry in "${entries[@]}"; do
         if [[ $window_title =~ $regex ]]; then
             echo "Window title: $window_title"
             echo "Regex pattern: $regex"
-
-            # Check if the regex contains special characters
-            if [[ $regex =~ [.*\[\](){}+^$\\|] ]]; then
-                echo "Regex contains special characters"
-                # Perform action for windows matching regex with special characters
-                # shellcheck disable=SC2046
-                kill -STOP $(pidof "$window_title")
-            else
-            # Get the PID associated with the current window class
-                additional_matches=$(xdotool search --class "$window_title")
-                # echo "Additional kill: $window_title pid: $additional_matches" 
-                # Pause the process
-                for match in $additional_matches; do 
-                    echo "Additionall kill: $match"
-                    kill -STOP "$additional_matches";
-                done 
-                
-            fi
+            echo STOP $window_title
+            kill -STOP $(pidof $window_title)
         fi
     done
 done
-
 
 # Print the array of matching search results
 echo "Matching search results: ${matching_results[@]}"
@@ -56,54 +40,46 @@ echo "Matching search results: ${matching_results[@]}"
 
 ### STOP and CONT all windows that match criteria always.
 
-# while true; do 
-#     cur=$(xdotool getactivewindow getwindowclassname)
+while true; do 
+    cur=$(xdotool getactivewindow getwindowclassname)
 
-#     if [[ $cur != $prev_matched ]]; then
-#         echo "Previous matched window class: $prev_matched"
-#         echo "Current window class: $cur"
-#         if [[ -n $prev_matched ]]; then
-#             if ! $process_stopped; then
-#                 echo "Stopping previous process..."
-#                 if pkill -STOP "$prev_matched"; then
-#                    pids="$(xdotool search --class $cur| xargs)"
-#                     for pid in $pids; do
-#                         killall -STOP "$pid"
-#                     done
-#                     echo "Process stopped successfully."
-#                     process_stopped=true
-#                 else
-#                     echo "Failed to stop process."
-#                 fi
-#             fi
-#         fi
-#     fi
+    if [[ $cur != $prev_matched ]]; then
+        echo "Previous matched window class: $prev_matched"
+        echo "Current window class: $cur"
+        if [[ -n $prev_matched ]]; then
+            if ! $process_stopped; then
+                echo "Stopping previous process..."
+                if pkill -STOP "$prev_matched"; then
+                    echo "Process stopped successfully."
+                    process_stopped=true
+                else
+                    echo "Failed to stop process."
+                fi
+            fi
+        fi
+    fi
 
-#     if [[ $cur == $prev_matched ]]; then
-#         echo "Resuming previously stopped process..."
-#            pids="$(xdotool search --class $cur| xargs)"
-#                 for pid in $pids; do
-#                     killall -CONT "$pid"
-#                 done
-#         if pkill -CONT "$cur"; then
-#             echo "Process resumed successfully."
-#             process_stopped=false
-#         else
-#             echo "Failed to resume process."
-#         fi
-#     fi
+    if [[ $cur == $prev_matched ]]; then
+        echo "Resuming previously stopped process..."
+        if pkill -CONT "$cur"; then
+            echo "Process resumed successfully."
+            process_stopped=false
+        else
+            echo "Failed to resume process."
+        fi
+    fi
 
-#     for entry in "${entries[@]}"; do
-#         IFS=',' read -ra parts <<< "$entry"
-#         regex="${parts[1]}"
-#         if [[ "$cur" =~ $regex ]]; then
-#             if [[ $cur != $prev_matched ]]; then
-#                 echo "Match found for ${parts[0]}: $cur"
-#             fi
-#             prev_matched=$cur
-#             break
-#         fi
-#     done
+    for entry in "${entries[@]}"; do
+        IFS=',' read -ra parts <<< "$entry"
+        regex="${parts[1]}"
+        if [[ "$cur" =~ $regex ]]; then
+            if [[ $cur != $prev_matched ]]; then
+                echo "Match found for ${parts[0]}: $cur"
+            fi
+            prev_matched=$cur
+            break
+        fi
+    done
 
-#     sleep .3
-# done
+    sleep .3
+done
