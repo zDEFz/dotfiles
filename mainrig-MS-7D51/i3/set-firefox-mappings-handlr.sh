b5e9bbcb333de67a527b5ad7094449d439ab6627
bluetail <a-development+gitea@posteo.de>
2024-06-27 04:24:53 +0200

    update

--- mainrig-MS-7D51/i3/set-firefox-mappings-handlr.sh
+++ mainrig-MS-7D51/i3/set-firefox-mappings-handlr.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set-xdg-firefox-default() {
-    # List of common MIME types and protocols for Firefox
+set_xdg_firefox_default() {
     local mime_types=(
         application/x-extension-htm
         application/x-extension-html
@@ -9,21 +8,40 @@ set-xdg-firefox-default() {
         application/x-extension-xht
         application/x-extension-xhtml
         text/html
+        text/plain
+        text/xml
+        application/xml
+        application/xhtml+xml
+        application/pdf
+        application/json
+        image/gif
+        image/jpeg
+        image/png
+        image/svg+xml
         video/mp4
+        video/webm
+        video/ogg
+        audio/webm
+        audio/ogg
+        application/rss+xml
+        application/rdf+xml
+        application/atom+xml
+        application/vnd.mozilla.xul+xml
         x-scheme-handler/about
         x-scheme-handler/chrome
         x-scheme-handler/http
         x-scheme-handler/https
+        x-scheme-handler/ftp
+        x-scheme-handler/mailto
+        x-scheme-handler/magnet
     )
 
-    # Set Firefox as the default for common types
     for type in "${mime_types[@]}"; do
         handlr set "$type" firefox-default.desktop
     done
 
-    # Specific handler exceptions
     handlr set x-scheme-handler/discord-4557121697957+ firefox-default.desktop
     handlr set x-scheme-handler/etcher balena-etcher.desktop
 }
 
-set-xdg-firefox-default
+set_xdg_firefox_default
