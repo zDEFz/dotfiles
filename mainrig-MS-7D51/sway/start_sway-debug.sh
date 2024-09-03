389fe92c3686af7448683dc592e49b23557176f3
bluetail <a-development+gitea@posteo.de>
2024-09-03 12:00:00 +0200

    Tue Sep  3 12:00:00 PM CEST 2024

--- mainrig-MS-7D51/sway/start_sway-debug.sh
+++ mainrig-MS-7D51/sway/start_sway-debug.sh
@@ -3,7 +3,7 @@
 # Set environment variable to fix fullscreen adaptive sync with Xwayland
 # export WLR_DRM_NO_MODIFIERS=1
 export XDG_CURRENT_DESKTOP=sway
-export QT_QPA_PLATFORMTHEME=qt6ct
+export QT_QPA_PLATFORMTHEME=qt5ct
 export WAYLAND_DEBUG=1
 # Launch sway with the desired option
-exec sway -c ~/.config/sway/sway-default-config --debug 2>> /home/blu/sway.log
+exec sway -c $HOME/.config/sway/sway-default-config --debug 2>> $HOME/sway.log
