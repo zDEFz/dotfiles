66333c0c42257593c5ef9d5ae80ed334b2e1d0a5
bluetail <a-development+gitea@posteo.de>
2024-09-05 13:00:01 +0200

    Thu Sep  5 01:00:01 PM CEST 2024

--- mainrig-MS-7D51/sway/start_sway.sh
+++ mainrig-MS-7D51/sway/start_sway.sh
@@ -4,6 +4,6 @@
 # export WLR_DRM_NO_MODIFIERS=1
 export XDG_CURRENT_DESKTOP=sway
 export QT_QPA_PLATFORMTHEME=qt5ct
-export WAYLAND_DEBUG=1
+# export WAYLAND_DEBUG=1
 # Launch sway with the desired option
 exec sway -c ~/.config/sway/config --debug 2>> /home/blu/sway.log
