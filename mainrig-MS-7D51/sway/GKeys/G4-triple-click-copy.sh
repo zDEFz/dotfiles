00f3847ea8028f65c5b398ff47bd8d08ef2c440b
bluetail <a-development+gitea@posteo.de>
2024-09-12 13:00:00 +0200

    Thu Sep 12 01:00:00 PM CEST 2024

--- mainrig-MS-7D51/sway/GKeys/G4-triple-click-copy.sh
+++ mainrig-MS-7D51/sway/GKeys/G4-triple-click-copy.sh
@@ -4,16 +4,19 @@
 dotoold &
 
 # Simulate three left clicks with a short delay
-for i in 1 2 3; do echo click left; sleep 0.05; done | dotoolc
+#for i in 1 2 3; do echo click left; sleep 0.05; done | dotoolc
 
 # Simulate Ctrl+C key press
 for i in 1 2; do
-echo key ctrl+c | dotoolc
-swaymsg '[class="code-oss"] focus'
-sleep 0.1
-echo key ctrl+v | dotoolc
-echo key enter  | dotoolc
-; done
+for i in 1 2 3; do echo click left; sleep 0.05; done | dotoolc
+
+  echo key ctrl+c | dotoolc
+  swaymsg '[class="code-oss"] focus'
+  sleep 0.1
+  echo key ctrl+v | dotoolc
+  echo key enter | dotoolc
+done
+
 #echo key ctrl+t | dotoolc
 #echo key ctrl+l | dotoolc
 #echo key ctrl+v | dotoolc
