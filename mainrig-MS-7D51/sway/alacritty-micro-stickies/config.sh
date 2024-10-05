218a71ff500ec358c2824a07ca7b824c37b83dab
bluetail <a-development+gitea@posteo.de>
2024-10-05 09:00:01 +0200

    Sat Oct  5 09:00:01 AM CEST 2024

---
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+script=./micro-stickies2.sh
+$script blue jap-cheat --startup
+$script orange firefox-tweaks --startup
+$script purple todo --startup
---
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/micro-stickies.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Set the color variable based on user input (blue or orange)
+
+micro_args="-colorscheme $1"
+mode="$2"
+pos_args=""
+workspace="$3"
+
+echo $3
+
+if [[ $mode == "--startup" ]]; then
+  # swaymsg "focus output 'BNQ ZOWIE XL LCD EBMCM01300SL0"
+    swaymsg workspace $3
+fi
+
+if [[ $mode == "--cursor" ]]; then
+  pos_args="move position cursor"
+fi
+
+# Generate a unique ID using the current timestamp and a random hex value
+unique_id=$(printf "%x" $(( $(date +%s%N) + RANDOM )))
+
+# Specify the width and height for the Alacritty window
+width=394
+height=527
+
+# Launch Alacritty with the unique ID as part of the class name and disown the process
+alacritty --working-directory="/home/blu/notes" \
+--class="alacritty-micro-$unique_id" \
+-e micro $micro_args &
+
+# Give Alacritty time to launch
+sleep .3
+
+# Move and resize the newly created Alacritty window to the cursor position
+swaymsg "[app_id=\"alacritty-micro-$unique_id\"] floating enable, resize set ${width} ${height}, ${pos_args}, border none"
