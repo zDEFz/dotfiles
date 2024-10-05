c7f91841ea11539b374f46006899effbaaebc01b
bluetail <a-development+gitea@posteo.de>
2024-10-05 10:00:00 +0200

    Sat Oct  5 10:00:00 AM CEST 2024

---
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#sleep 10
+script=/home/blu/.config/sway/alacritty-micro-stickies/micro-stickies.sh
+$script "blue jap-cheat" --startup "24"
+# $script "orange firefox-tweaks" --startup "1"
+$script "purple todo" --startup "1"
