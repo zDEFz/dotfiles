a40eb43ddad5d42ddbb640a4f88a62cf23920542
bluetail <a-development+gitea@posteo.de>
2024-10-07 10:00:00 +0200

    Mon Oct  7 10:00:00 AM CEST 2024

--- mainrig-MS-7D51/sway/start_sway.sh
+++ mainrig-MS-7D51/sway/start_sway.sh
@@ -7,6 +7,6 @@ export QT_QPA_PLATFORMTHEME=qt5ct
 export WLR_RENDERER=vulkan
 # export WAYLAND_DEBUG=1
 # Launch sway with the desired option
-exec sway -c /home/blu/.config/sway/config
+exec dbus-run-session sway -c /home/blu/.config/sway/config
 #--debug 2
 # >> /home/blu/sway.log
