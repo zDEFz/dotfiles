5fb124c874eb596929c6856d44fd7b20c648c0ef
bluetail <a-development+gitea@posteo.de>
2024-10-08 19:00:00 +0200

    Tue Oct  8 07:00:00 PM CEST 2024

--- mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 #sleep 10
 script=/home/blu/.config/sway/alacritty-micro-stickies/micro-stickies.sh
-   
-    # $script "purple todo"                                   --startup    "1"
-    #$script "blue jap-cheat" --startup "24" "2607 853"
+
+# Wanikani bottom right
+$script "blue jap-cheat" --startup "24" "2607 853"
 
     # $script "orange firefox-tweaks"                         --startup    "1"
 
 # First Row
-    $script "purple mind"                                   --startup    "52" "1077 268"
-    $script "red health"                                    --startup    "52" "1469 268"
-    
-    $script "orange sap-talk-become"                        --startup    "52" "1861 268"
-    $script "orange sap-talk-myself"                        --startup    "52" "2253 268"
-    $script "orange sap-talk-I-am"                          --startup    "52" "2645 268"
+
+    $script "orange sap-talk-I-was"                         --startup    "52" "1077 268"
+    $script "orange sap-talk-become"                         --startup    "52" "1469 268"
+    $script "orange sap-talk-myself"                         --startup    "52" "1861 268"
+    $script "orange sap-talk-I-am"                           --startup    "52" "2253 268"      
 
 # Second Row
-     $script "orange sap-talk-I-was"                         --startup    "52" "1077 785"
-     $script "transparent groceries"                           --startup    "52" "1469 785"
+    $script "purple mind"                                   --startup    "52" "1077 768"
+    $script "red health"                                    --startup    "52" "1469 768"
+    $script "transparent groceries"                         --startup    "52" "1861 768"
+    $script "purple todo"                                   --startup    "52" "2253 768"
+
+
+    
