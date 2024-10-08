27309e45a5be2e34ac59d254ee6920a375848bf5
bluetail <a-development+gitea@posteo.de>
2024-10-08 23:00:01 +0200

    Tue Oct  8 11:00:01 PM CEST 2024

--- mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
@@ -5,20 +5,17 @@ script=/home/blu/.config/sway/alacritty-micro-stickies/micro-stickies.sh
 # Wanikani bottom right
 $script "blue jap-cheat" --startup "24" "2607 853"
 
-    # $script "orange firefox-tweaks"                         --startup    "1"
+    # $script "transparent firefox-tweaks"                         --startup    "1"
 
 # First Row
 
-    $script "orange sap-talk-I-was"                         --startup    "52" "1077 268"
-    $script "orange sap-talk-become"                         --startup    "52" "1469 268"
-    $script "orange sap-talk-myself"                         --startup    "52" "1861 268"
-    $script "orange sap-talk-I-am"                           --startup    "52" "2253 268"      
+    $script "transparent sap-talk-I-was"                         --startup    "52" "1077 268"
+    $script "transparent sap-talk-become"                         --startup    "52" "1469 268"
+    $script "transparent sap-talk-myself"                         --startup    "52" "1861 268"
+    $script "transparent sap-talk-I-am"                           --startup    "52" "2253 268"      
 
 # Second Row
-    $script "purple mind"                                   --startup    "52" "1077 768"
-    $script "red health"                                    --startup    "52" "1469 768"
+    $script "transparent mind"                                   --startup    "52" "1077 768"
+    $script "transparent health"                                    --startup    "52" "1469 768"
     $script "transparent groceries"                         --startup    "52" "1861 768"
-    $script "purple todo"                                   --startup    "52" "2253 768"
-
-
-    
+    $script "transparent todo"                                   --startup    "52" "2253 768"    
