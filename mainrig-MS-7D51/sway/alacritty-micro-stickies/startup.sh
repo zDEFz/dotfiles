85f8f95cac04f849652f7d48695416e5334fe6b0
bluetail <a-development+gitea@posteo.de>
2024-10-09 13:00:00 +0200

    Wed Oct  9 01:00:00 PM CEST 2024

--- mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sleep 20
+#sleep 20
 script=/home/blu/.config/sway/alacritty-micro-stickies/micro-stickies.sh
 
 # Wanikani bottom right
@@ -9,14 +9,14 @@ $script "transparent jap-cheat" --startup "24" "2607 853"
 
 # First Row
 
-    $script "transparent sap-talk-I-was"                         --startup    "52" "1263 325"
-    $script "transparent sap-talk-become"                         --startup    "52" "1649 325"
-    $script "transparent sap-talk-myself"                         --startup    "52" "2041 325"
-    $script "transparent sap-talk-I-am"                           --startup    "52" "2433 325"      
+    $script "transparent sap-talk-I-was"                         --startup    "52" "1257 325"
+    $script "transparent sap-talk-become"                         --startup    "52" "1643 325"
+    $script "transparent sap-talk-myself"                         --startup    "52" "2029 325"
+    $script "transparent sap-talk-I-am"                           --startup    "52" "2415 325"      
 
 # # Second Row
-    $script "transparent mind"                                   --startup    "52" "1077 820"
+    $script "transparent mind"                                   --startup    "52" "1083 820"
     $script "transparent health"                                    --startup    "52" "1469 820"
-    $script "transparent groceries"                         --startup    "52" "1861 820"
-    $script "transparent todo"                                   --startup    "52" "2253 820"    
-    $script "transparent workout"                                   --startup    "52" "2645 820"
+    $script "transparent groceries"                         --startup    "52" "1855 820"
+    $script "transparent todo"                                   --startup    "52" "2241 820"    
+    $script "transparent workout"                                   --startup    "52" "2627 820"
