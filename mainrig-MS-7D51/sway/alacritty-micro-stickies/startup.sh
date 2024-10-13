c14d0a3bb8b46f3b1acf4e8227ac0d994fd82dd7
bluetail <a-development+gitea@posteo.de>
2024-10-13 16:00:00 +0200

    Sun Oct 13 04:00:00 PM CEST 2024

--- mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
@@ -3,20 +3,20 @@
 script=/home/blu/.config/sway/alacritty-micro-stickies/micro-stickies.sh
 
 # Wanikani bottom right
-$script "transparent jap-cheat" --startup "24" "2607 853"
+$script "transparent jap-cheat" --startup "24" "3701 952"
 
     # $script "transparent firefox-tweaks"                         --startup    "1"
 
 # First Row
 
-    $script "transparent sap-talk-I-was"                         --startup    "52" "1257 325"
-    $script "transparent sap-talk-become"                         --startup    "52" "1643 325"
-    $script "transparent sap-talk-myself"                         --startup    "52" "2029 325"
-    $script "transparent sap-talk-I-am"                           --startup    "52" "2415 325"      
+    $script "transparent sap-talk-I-was"                         --startup    "52" "2337 325"
+    $script "transparent sap-talk-become"                         --startup    "52" "2723 325"
+    $script "transparent sap-talk-myself"                         --startup    "52" "3109 325"
+    $script "transparent sap-talk-I-am"                           --startup    "52" "3495 325"      
 
-# # Second Row
-    $script "transparent mind"                                   --startup    "52" "1083 820"
-    $script "transparent health"                                    --startup    "52" "1469 820"
-    $script "transparent groceries"                         --startup    "52" "1855 820"
-    $script "transparent todo"                                   --startup    "52" "2241 820"    
-    $script "transparent workout"                                   --startup    "52" "2627 820"
+# # # Second Row
+    $script "transparent mind"                                   --startup    "52" "2163 820"
+    $script "transparent health"                                    --startup    "52" "2549 820"
+    $script "transparent groceries"                             --startup    "52" "2935 820"
+    $script "transparent todo"                                   --startup    "52" "3321 820"    
+    $script "transparent workout"                                   --startup    "52" "3707 820"
