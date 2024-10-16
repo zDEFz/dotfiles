4a25c8bd939d7f7c6f1510ee2201db56d821411b
bluetail <a-development+gitea@posteo.de>
2024-10-16 11:00:00 +0200

    Wed Oct 16 11:00:00 AM CEST 2024

---
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/startup-now.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#sleep 15
+script=/home/blu/.config/sway/alacritty-micro-stickies/micro-stickies.sh
+
+# Wanikani bottom right
+$script "transparent jap-cheat" --startup "24" "3701 952"
+
+    # $script "transparent firefox-tweaks"                         --startup    "1"
+
+# First Row
+
+    $script "transparent sap-talk-I-was"                         --startup    "52" "2337 325"
+    $script "transparent sap-talk-become"                         --startup    "52" "2723 325"
+    $script "transparent sap-talk-myself"                         --startup    "52" "3109 325"
+    $script "transparent sap-talk-I-am"                           --startup    "52" "3495 325"      
+
+# # # Second Row
+    $script "transparent mind"                                   --startup    "52" "2163 820"
+    $script "transparent health"                                    --startup    "52" "2549 820"
+    $script "transparent groceries"                             --startup    "52" "2935 820"
+    $script "transparent todo"                                   --startup    "52" "3321 820"    
+    $script "transparent workout"                                   --startup    "52" "3707 820"
