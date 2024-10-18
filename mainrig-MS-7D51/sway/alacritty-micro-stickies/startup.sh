ba3b59229bd0a17c3d239117dfc91721dedc89f4
bluetail <a-development+gitea@posteo.de>
2024-10-18 23:00:00 +0200

    Fri Oct 18 11:00:00 PM CEST 2024

--- mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
@@ -1,41 +1,53 @@
 #!/bin/bash
-#sleep 15
+sleep 15
 script=/home/blu/.config/sway/alacritty-micro-stickies/micro-stickies.sh
 
-## LL Screen
-
-
-
-
 # Wanikani bottom right
-# $script "transparent jap-cheat" --startup "24" "3701 952"
-
-    # $script "transparent firefox-tweaks"                         --startup    "1"
-
+    $script "transparent jap-cheat" --startup "24" "3581 949"
 
 # LL screen
 
-portrait=1080
-landscape=1920
+    LL_SCREEN_pos01_portrait="0 0"                 
+    LL_SCREEN_pos02_portrait="500 0"               
 
-# Define positions using the portrait and landscape dimensions
-pos01_portrait="0 0"                   # First row, first column
-pos02_portrait="500 0"                 # First row, second column
-pos10_portrait="0 500"                 # Second row, first column
-pos11_portrait="500 500"               # Second row, second column
-pos10_portrait="0 1000"                 # Third row, first column
-pos11_portrait="500 1000"               # Third row, second colum
+    LL_SCREEN_pos10_portrait="0 500"               
+    LL_SCREEN_pos11_portrait="500 500"             
 
+    LL_SCREEN_pos20_portrait="0 1000"              
+    LL_SCREEN_pos21_portrait="500 1000"            
 
-$script "transparent sap-talk-I-was"    --startup "00" "$pos01_portrait"
-$script "transparent sap-talk-become"   --startup "00" "$pos02_portrait"
-$script "transparent sap-talk-myself"   --startup "00" "$pos10_portrait"
-$script "transparent sap-talk-I-am"     --startup "00" "$pos11_portrait"
+    LL_SCREEN_pos30_portrait="0 1500"              
+    LL_SCREEN_pos31_portrait="500 1500"    
 
+    LL_SCREEN_pos40_portrait="0 2000"              
+    LL_SCREEN_pos41_portrait="500 2000"    
 
-# # # # Second Row
-#     $script "transparent mind"                                   --startup    "52" "2163 820"
-#     $script "transparent health"                                    --startup    "52" "2549 820"
-#     $script "transparent groceries"                             --startup    "52" "2935 820"
-#     $script "transparent todo"                                   --startup    "52" "3321 820"    
-#     $script "transparent workout"                                   --startup    "52" "3707 820"
+# RR screen
+    RR_SCREEN_pos01_portrait="4080 0"             
+    RR_SCREEN_pos02_portrait="4580 0"             
+
+    RR_SCREEN_pos10_portrait="4080 500"            
+    RR_SCREEN_pos11_portrait="4580 500"       
+
+    RR_SCREEN_pos20_portrait="4080 1000"           
+    RR_SCREEN_pos21_portrait="4580 1000"           
+
+    RR_SCREEN_pos30_portrait="4080 1500"           
+    RR_SCREEN_pos31_portrait="4580 1500"           
+
+    RR_SCREEN_pos40_portrait="4080 2000"           
+    RR_SCREEN_pos41_portrait="4580 2000"           
+
+$script "transparent sap-talk-I-was"    --startup "00" "$RR_SCREEN_pos01_portrait"
+$script "transparent sap-talk-become"   --startup "00" "$RR_SCREEN_pos02_portrait"
+
+$script "transparent sap-talk-myself"   --startup "00" "$RR_SCREEN_pos10_portrait"
+$script "transparent sap-talk-I-am"     --startup "00" "$RR_SCREEN_pos11_portrait"
+
+$script "transparent mind"              --startup "00" "$RR_SCREEN_pos20_portrait"
+$script "transparent health"            --startup "00" "$RR_SCREEN_pos21_portrait"
+
+$script "transparent groceries"         --startup "00" "$RR_SCREEN_pos30_portrait"
+$script "transparent todo  "            --startup "00" "$RR_SCREEN_pos31_portrait"
+    
+#     $script "transparent workout"                                 --startup    "52" "3707 820"
