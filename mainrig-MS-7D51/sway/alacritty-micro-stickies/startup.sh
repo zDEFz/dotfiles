cdfa48e978db71085498a71e94ad29909e744f08
bluetail <a-development+gitea@posteo.de>
2024-10-19 15:00:00 +0200

    Sat Oct 19 03:00:00 PM CEST 2024

--- mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
@@ -9,7 +9,7 @@ script=/home/blu/.config/sway/alacritty-micro-stickies/micro-stickies.sh
 
 # LL screen
 
-    LL_SCREEN_pos01_portrait="0   0"                 
+    LL_SCREEN_pos01_portrait="0   0"
     LL_SCREEN_pos02_portrait="640 0"               
 
     LL_SCREEN_pos10_portrait="0   500"               
@@ -43,23 +43,23 @@ script=/home/blu/.config/sway/alacritty-micro-stickies/micro-stickies.sh
 # R screen
 R_SCREEN_BOTTOM_RIGHT="3581 949"    
 
-$script "transparent sap-talk-I-was"    --startup "RR1" "$RR_SCREEN_pos01_portrait" &
+$script "transparent sap-talk-I-was"    --startup "4" "$RR_SCREEN_pos01_portrait" &
     sleep .25
-$script "transparent sap-talk-become"   --startup "RR1" "$RR_SCREEN_pos02_portrait" &
+$script "transparent sap-talk-become"   --startup "4" "$RR_SCREEN_pos02_portrait" &
     sleep .25
-$script "transparent sap-talk-myself"   --startup "RR1" "$RR_SCREEN_pos10_portrait" &
+$script "transparent sap-talk-myself"   --startup "4" "$RR_SCREEN_pos10_portrait" &
     sleep .25
-$script "transparent workout"           --startup "RR1" "$RR_SCREEN_pos11_portrait" &
+$script "transparent workout"           --startup "4" "$RR_SCREEN_pos11_portrait" &
     sleep .25
-$script "transparent mind"              --startup "RR1" "$RR_SCREEN_pos20_portrait" &
+$script "transparent mind"              --startup "4" "$RR_SCREEN_pos20_portrait" &
     sleep .25
-$script "transparent health"            --startup "RR1" "$RR_SCREEN_pos21_portrait" &
+$script "transparent health"            --startup "4" "$RR_SCREEN_pos21_portrait" &
     sleep .25
-$script "transparent groceries"         --startup "RR1" "$RR_SCREEN_pos30_portrait" &
+$script "transparent groceries"         --startup "4" "$RR_SCREEN_pos30_portrait" &
     sleep .25
-$script "transparent todo"              --startup "RR1" "$RR_SCREEN_pos31_portrait" &
+$script "transparent todo"              --startup "4" "$RR_SCREEN_pos31_portrait" &
     sleep .25
 # Wanikani bottom right
-    $script "transparent jap-cheat"     --startup "31"   "$R_SCREEN_BOTTOM_RIGHT" &
+    $script "blue jap-cheat"            --startup "31"   "$R_SCREEN_BOTTOM_RIGHT"   "600" "498" &
     
-#     $script "transparent workout"                                 --startup    "52" "3707 820"
+#     $script "transparent workout"                --startup    "52" "3707 820"
--- mainrig-MS-7D51/sway/ws-switch.sh
+++ mainrig-MS-7D51/sway/ws-switch.sh
@@ -1,24 +1,56 @@
 #!/bin/bash
-# Real current workspace
 
-# Get current workspace name and convert it to a number, then adjust for even or odd
-current_workspace=$(swaymsg -t get_workspaces | jq -r '.[] | select(.focused == true).name | tonumber | if . % 2 == 1 then . + 1 else . end')
-echo "Current workspace: $current_workspace"
+# Get the current focused workspace number
+current_workspace=$(swaymsg -t get_workspaces | jq -r '.[] | select(.focused == true) | .name | tonumber')
 
+# Group size (4 workspaces)
+group_size=4
+
+# Determine the current group
+current_group=$(( (current_workspace - 1) / group_size ))
+
+# Determine the next or previous group based on the argument
 if [ "$1" == "next" ]; then
-    nr=$((current_workspace + 2)) # Next on right monitor
-    nl=$((current_workspace + 1)) # Next on left monitor
-    swaymsg "workspace $nl; workspace $nr"
+    next_group=$((current_group + 1))
+else
+    next_group=$((current_group - 1))
 fi
 
-if [ "$1" == "prev" ]; then
-    nr=$((current_workspace - 2))
-    nl=$((current_workspace - 3))
-    echo "Previous right workspace: $nr"
-    echo "Previous left workspace: $nl"
-    if [ $nl -gt -1 ]; then
-        swaymsg "workspace $nl; workspace $nr"
-    else
-        echo "Previous left workspace is out of bounds"
-    fi
+# Calculate the starting workspace for the next or previous group
+start_workspace=$((next_group * group_size + 1))
+
+# Check for bounds (ensuring we don't go below 1 or exceed available workspaces)
+total_workspaces=54  # Adjust this value based on your setup
+
+if [ "$start_workspace" -le 0 ]; then
+    echo "You are already at the first group."
+    exit 1
 fi
+
+if [ "$start_workspace" -gt "$total_workspaces" ]; then
+    echo "No more workspaces available."
+    exit 1
+fi
+
+# Create the list of workspaces to switch to
+workspace_list=""
+
+for ((i = 0; i < group_size; i++)); do
+    workspace_num=$((start_workspace + i))
+    if [ "$workspace_num" -le "$total_workspaces" ]; then
+        if [ -n "$workspace_list" ]; then
+            workspace_list+=", "
+        fi
+        workspace_list+="workspace $workspace_num"
+    fi
+done
+
+# Switch to the specified workspaces
+swaymsg "$workspace_list"
+
+# Focus the middle one of the new group
+middle_workspace=$((start_workspace + (group_size / 2)))
+swaymsg "workspace $middle_workspace"
+
+# Output the workspace list
+echo "Switched to: $workspace_list"
