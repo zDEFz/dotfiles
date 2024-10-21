1cde486b3cab960f894d80069959a04a3a44d7e7
bluetail <a-development+gitea@posteo.de>
2024-10-21 03:00:00 +0200

    Mon Oct 21 03:00:00 AM CEST 2024

--- mainrig-MS-7D51/sway/alacritty-micro-stickies/micro-stickies.sh
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/micro-stickies.sh
@@ -3,37 +3,75 @@
 # Set the color variable based on user input (blue or orange)
 micro_args="-colorscheme $1"
 mode="$2"
-pos_args=""
 workspace="$3"
+name=""
+
+# Start overall time measurement
+total_start_time=$(date +%s.%N)
+
+# Start time measurement for unique ID generation
+unique_id_start_time=$(date +%s.%N)
+
+# Generate a unique ID using the current timestamp and a random hex value (non-blocking)
+unique_id=$( (timestamp=$(date +%s); random_hex=$(printf "%x" $((RANDOM))); echo "${timestamp}-${random_hex}") & wait $!)
+
+# End time measurement for unique ID generation
+unique_id_end_time=$(date +%s.%N)
+unique_id_elapsed_time=$(echo "$unique_id_end_time - $unique_id_start_time" | bc)
+
+# Output unique ID generation time
+echo "Unique ID generation time: $unique_id_elapsed_time seconds"
 
 if [[ $mode == "--startup" ]]; then
-   swaymsg "focus output 'BNQ ZOWIE XL LCD EBMCM01300SL0"
-   swaymsg workspace $3
-   sleep .1
-   pos_args="move absolute position $4"
+    name="alacritty-micro-startup-"
+    
+    # Focus output and switch workspace in a single command
+    swaymsg "focus output 'BNQ ZOWIE XL LCD EBMCM01300SL0'" &
+    swaymsg "workspace $workspace" &
+
+    # Start time measurement for launching Alacritty
+    alacritty_start_time=$(date +%s.%N)
+
+    # Launch Alacritty with the unique ID as part of the class name and disown the process
+    alacritty \
+    --class="${name}${unique_id}" \
+    -e micro $micro_args &
+
+    # Give Alacritty time to launch
+    sleep 0.12
+
+    # Move the newly created Alacritty window to the specified position
+    swaymsg "[app_id=\"${name}${unique_id}\"] move absolute position $4" &
+
+    # End time measurement for launching Alacritty
+    alacritty_end_time=$(date +%s.%N)
+    alacritty_elapsed_time=$(echo "$alacritty_end_time - $alacritty_start_time" | bc)
+
+    # Output Alacritty launch time
+    echo "Alacritty launch time: $alacritty_elapsed_time seconds"
 fi
 
 if [[ $mode == "--cursor" ]]; then
-  pos_args="move position cursor"
+    name="alacritty-micro-cursor-"
+    
+    # Start time measurement for launching Alacritty in cursor mode
+    cursor_start_time=$(date +%s.%N)
+    
+    alacritty \
+    --class="${name}${unique_id}" \
+    -e micro $micro_args &
+
+    # End time measurement for launching Alacritty in cursor mode
+    cursor_end_time=$(date +%s.%N)
+    cursor_elapsed_time=$(echo "$cursor_end_time - $cursor_start_time" | bc)
+
+    # Output cursor mode launch time
+    echo "Cursor mode Alacritty launch time: $cursor_elapsed_time seconds"
 fi
 
-# Generate a unique ID using the current timestamp and a random hex value
-timestamp=$(date +%s)
-random_hex=$(printf "%x" $((RANDOM)))
-unique_id="${timestamp}-${random_hex}"
+# End overall time measurement
+total_end_time=$(date +%s.%N)
+total_elapsed_time=$(echo "$total_end_time - $total_start_time" | bc)
 
-# Specify the width and height for the Alacritty window
-width=500
-height=498
-
-# Launch Alacritty with the unique ID as part of the class name and disown the process
-alacritty \
---class="alacritty-micro-$unique_id" \
--e micro $micro_args &
-
-
-# Give Alacritty time to launch
-sleep .4
-
-# Move and resize the newly created Alacritty window to the cursor position or specified position
-swaymsg "[app_id=\"alacritty-micro-$unique_id\"] floating enable, resize set ${width} ${height}, ${pos_args}, border none"
+# Output total execution time
+echo "Total script execution time: $total_elapsed_time seconds"
--- mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/startup.sh
@@ -25,43 +25,43 @@ script=/home/blu/.config/sway/alacritty-micro-stickies/micro-stickies.sh
     LL_SCREEN_pos41_portrait="640 2000"    
 
 # RR screen
-    RR_SCREEN_pos01_portrait="4080 14"             
-    RR_SCREEN_pos02_portrait="4670 14"             
+    RR_SCREEN_pos01_portrait="4080 42"             
+    RR_SCREEN_pos02_portrait="4670 42"             
 
-    RR_SCREEN_pos10_portrait="4080 514"            
-    RR_SCREEN_pos11_portrait="4670 512"       
+    RR_SCREEN_pos10_portrait="4080 542"            
+    RR_SCREEN_pos11_portrait="4670 542"       
 
-    RR_SCREEN_pos20_portrait="4080 1014"           
-    RR_SCREEN_pos21_portrait="4670 1014"           
+    RR_SCREEN_pos20_portrait="4080 1042"           
+    RR_SCREEN_pos21_portrait="4670 1042"           
 
-    RR_SCREEN_pos30_portrait="4080 1514"           
-    RR_SCREEN_pos31_portrait="4670 1514"           
+    RR_SCREEN_pos30_portrait="4080 1542"           
+    RR_SCREEN_pos31_portrait="4670 1542"           
 
-    RR_SCREEN_pos40_portrait="4080 2014"           
-    RR_SCREEN_pos41_portrait="4670 2014"           
+    RR_SCREEN_pos40_portrait="4080 2042"           
+    RR_SCREEN_pos41_portrait="4670 2042"           
 
 # R screen
 R_SCREEN_BOTTOM_RIGHT="3581 949"    
 
 p="/home/blu/notes"
 
-$script "transparent ${p}/ws-04/sap-talk-I-was"    --startup "4" "$RR_SCREEN_pos01_portrait" &
-    sleep .25
-$script "transparent ${p}/ws-04/sap-talk-become"   --startup "4" "$RR_SCREEN_pos02_portrait" &
-    sleep .25
-$script "transparent ${p}/ws-04/sap-talk-myself"   --startup "4" "$RR_SCREEN_pos10_portrait" &
-    sleep .25
-$script "transparent ${p}/ws-04/workout"           --startup "4" "$RR_SCREEN_pos11_portrait" &
-    sleep .25
-$script "transparent ${p}/ws-04/mind"              --startup "4" "$RR_SCREEN_pos20_portrait" &
-    sleep .25
-$script "transparent ${p}/ws-04/health"            --startup "4" "$RR_SCREEN_pos21_portrait" &
-    sleep .25
-$script "transparent ${p}/ws-04/groceries"         --startup "4" "$RR_SCREEN_pos30_portrait" &
-    sleep .25
-$script "transparent ${p}/ws-04/todo"              --startup "4" "$RR_SCREEN_pos31_portrait" &
-    sleep .25
+$script "transparent ${p}/ws-04/sap-talk-I-was"    --startup "4" "$RR_SCREEN_pos01_portrait" & sleep .12
+    
+$script "transparent ${p}/ws-04/sap-talk-become"   --startup "4" "$RR_SCREEN_pos02_portrait" & sleep .12
+    
+$script "transparent ${p}/ws-04/sap-talk-myself"   --startup "4" "$RR_SCREEN_pos10_portrait" & sleep .12
+    
+$script "transparent ${p}/ws-04/workout"           --startup "4" "$RR_SCREEN_pos11_portrait" & sleep .12
+    
+$script "transparent ${p}/ws-04/mind"              --startup "4" "$RR_SCREEN_pos20_portrait" & sleep .12
+    
+$script "transparent ${p}/ws-04/health"            --startup "4" "$RR_SCREEN_pos21_portrait" & sleep .12
+    
+$script "transparent ${p}/ws-04/groceries"         --startup "4" "$RR_SCREEN_pos30_portrait" & sleep .12
+    
+$script "transparent ${p}/ws-04/todo"              --startup "4" "$RR_SCREEN_pos31_portrait" & sleep .12
+    
 # Wanikani bottom right
-$script "blue ${p}/ws-31/jap-cheat"                --startup "31"   "$R_SCREEN_BOTTOM_RIGHT" &
+$script "blue ${p}/ws-31/jap-cheat"                --startup "31"   "$R_SCREEN_BOTTOM_RIGHT" & sleep .12
     
 #     $script "transparent workout"                --startup    "52" "3707 820"
