eae98e620b42a70465626b7263477b0a511fb284
bluetail <a-development+gitea@posteo.de>
2024-10-21 15:00:00 +0200

    Mon Oct 21 03:00:00 PM CEST 2024

--- mainrig-MS-7D51/sway/alacritty-micro-stickies/micro-stickies.sh
+++ mainrig-MS-7D51/sway/alacritty-micro-stickies/micro-stickies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the color variable based on user input (blue or orange)
-micro_args="-colorscheme $1"
+micro_args="-colorscheme $1 -encoding shift_jis"
 mode="$2"
 workspace="$3"
 name=""
