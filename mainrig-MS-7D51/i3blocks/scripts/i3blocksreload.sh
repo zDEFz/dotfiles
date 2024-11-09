d8d3317ad11d0c039c4b40e5d622628552f78a6c
bluetail <a-development+gitea@posteo.de>
2024-11-09 09:00:01 +0100

    Sat Nov  9 09:00:00 AM CET 2024

--- mainrig-MS-7D51/i3blocks/scripts/i3blocksreload.sh
+++ mainrig-MS-7D51/i3blocks/scripts/i3blocksreload.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 killall i3bar
 i3-msg "exec --no-startup-id i3bar --bar_id=bar-1"
-i3-msg "exec --no-startup-id  i3bar --bar_id=bar-2"
+i3-msg "exec --no-startup-id i3bar --bar_id=bar-2"
+i3-msg "exec --no-startup-id i3bar --bar_id=bar-3"
+i3-msg "exec --no-startup-id i3bar --bar_id=bar-4"
