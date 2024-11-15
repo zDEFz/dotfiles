fa21605f7984e28a9e75ba781544e23fc163bdef
bluetail <a-development+gitea@posteo.de>
2024-11-15 18:00:01 +0100

    Fri Nov 15 06:00:01 PM CET 2024

--- mainrig-MS-7D51/sway/start_sway.sh
+++ mainrig-MS-7D51/sway/start_sway.sh
@@ -2,21 +2,20 @@
 
 # Set environment variable to fix fullscreen adaptive sync with Xwayland
 # export WLR_DRM_NO_MODIFIERS=1
-export XDG_CURRENT_DESKTOP=sway
-export QT_QPA_PLATFORMTHEME=qt5ct
 export GTK_THEME=Breeze:dark
+export QT_QPA_PLATFORMTHEME=qt5ct
 export XDG_CONFIG_HOME="$HOME/.config"
-
-
+export XDG_CURRENT_DESKTOP=sway
+# export XWAYLAND_NO_GLAMOR=1
 
 # Single GPU
 # export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT
 # Dual GPU with AMD 6950XT as renderer
 export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT:/dev/dri/by-name/NVIDIA_2000e_Ada
 export WLR_NO_HARDWARE_CURSORS=1
-
 export WLR_RENDERER=vulkan
+
 # export WAYLAND_DEBUG=1
 # Launch sway with the desired option
-exec sway -c /home/blu/.config/sway/config --unsupported-gpu
 # --debug 2>> /home/blu/sway.log
+exec sway -c /home/blu/.config/sway/config --unsupported-gpu
