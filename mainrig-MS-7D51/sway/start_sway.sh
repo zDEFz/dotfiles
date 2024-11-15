efd16ed8daa43b84e0fa16e9b2e3399850e3cba8
bluetail <a-development+gitea@posteo.de>
2024-11-15 20:00:00 +0100

    Fri Nov 15 08:00:00 PM CET 2024

--- mainrig-MS-7D51/sway/start_sway.sh
+++ mainrig-MS-7D51/sway/start_sway.sh
@@ -10,8 +10,11 @@ export XDG_CURRENT_DESKTOP=sway
 
 # Single GPU
 # export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT
-# Dual GPU with AMD 6950XT as renderer
-export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT:/dev/dri/by-name/NVIDIA_2000e_Ada
+# Dual GPU with AMD 6950XT as main renderer
+	  export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT:/dev/dri/by-name/NVIDIA_2000e_Ada
+# Dual GPU with NVIDIA_2000e_Ada as main renderer - may require displays to be connected to ADA GPU
+	# export WLR_DRM_DEVICES=/dev/dri/by-name/NVIDIA_2000e_Ada:/dev/dri/by-name/AMD_6950XT
+	
 export WLR_NO_HARDWARE_CURSORS=1
 export WLR_RENDERER=vulkan
 
