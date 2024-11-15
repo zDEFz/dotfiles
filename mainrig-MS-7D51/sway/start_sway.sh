498a3ae0dfba9f002d860ef3e9a69a142d664293
bluetail <a-development+gitea@posteo.de>
2024-11-15 22:00:00 +0100

    Fri Nov 15 10:00:00 PM CET 2024

--- mainrig-MS-7D51/sway/start_sway.sh
+++ mainrig-MS-7D51/sway/start_sway.sh
@@ -11,14 +11,15 @@ export XDG_CURRENT_DESKTOP=sway
 # Single GPU
 # export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT
 # Dual GPU with AMD 6950XT as main renderer
-	  export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT:/dev/dri/by-name/NVIDIA_2000e_Ada
+ export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT:/dev/dri/by-name/NVIDIA_2000e_Ada
 # Dual GPU with NVIDIA_2000e_Ada as main renderer - may require displays to be connected to ADA GPU
-	# export WLR_DRM_DEVICES=/dev/dri/by-name/NVIDIA_2000e_Ada:/dev/dri/by-name/AMD_6950XT
+ # export WLR_DRM_DEVICES=/dev/dri/by-name/NVIDIA_2000e_Ada:/dev/dri/by-name/AMD_6950XT
 	
 export WLR_NO_HARDWARE_CURSORS=1
-export WLR_RENDERER=vulkan
+# export WLR_RENDERER=vulkan
 
 # export WAYLAND_DEBUG=1
 # Launch sway with the desired option
 # --debug 2>> /home/blu/sway.log
 exec sway -c /home/blu/.config/sway/config --unsupported-gpu
+# --debug 2>> /home/blu/sway2.log
