acc5dd7a12c8aebf1c2945e1627708897436788d
bluetail <a-development+gitea@posteo.de>
2024-11-15 10:00:01 +0100

    Fri Nov 15 10:00:01 AM CET 2024

--- mainrig-MS-7D51/sway/start_sway.sh
+++ mainrig-MS-7D51/sway/start_sway.sh
@@ -6,6 +6,11 @@ export XDG_CURRENT_DESKTOP=sway
 export QT_QPA_PLATFORMTHEME=qt5ct
 export GTK_THEME=Breeze:dark
 export XDG_CONFIG_HOME="$HOME/.config"
+# Single GPU
+export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT
+# Dual GPU with AMD 6950XT as renderer
+# export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT:/dev/dri/by-name/NVIDIA_2000e_Ada
+
 # export WLR_RENDERER=vulkan
 # export WAYLAND_DEBUG=1
 # Launch sway with the desired option
