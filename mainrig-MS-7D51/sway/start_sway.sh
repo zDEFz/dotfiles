4cc47bd3d5a9185baae280d30d9cdd60e9a4b3ad
bluetail <a-development+gitea@posteo.de>
2024-11-18 00:00:00 +0100

    Mon Nov 18 12:00:00 AM CET 2024

--- mainrig-MS-7D51/sway/start_sway.sh
+++ mainrig-MS-7D51/sway/start_sway.sh
@@ -8,6 +8,7 @@ export GTK_THEME=Breeze:dark
 export QT_QPA_PLATFORMTHEME=qt5ct
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CURRENT_DESKTOP=sway
+export _JAVA_AWT_WM_NONREPARENTING=1
 # AMD 6950XT as main renderer RX 6400 as secondary
 export WLR_DRM_DEVICES=/dev/dri/by-name/AMD_6950XT:/dev/dri/by-name/AMD_RX_6400
 # Start sway
