ff20d09fae31529ac57b9e05f095ea94c53c4e24
bluetail <a-development+gitea@posteo.de>
2024-11-26 11:00:00 +0100

    Tue Nov 26 11:00:00 AM CET 2024

--- mainrig-MS-7D51/sway/scripts/mediacontrol.sh
+++ mainrig-MS-7D51/sway/scripts/mediacontrol.sh
@@ -1,42 +1,35 @@
 #!/bin/bash
-MPV_SOCKET="/tmp/mpvsocket"
-
-# Function to handle errors with mpv and fallback to playerctl
-handle_mpv_error() {
-  if ! socat - "$MPV_SOCKET" <<< "ping" &>/dev/null; then
-    echo "MPV error or socket unavailable, switching to playerctl."
-    playerctl "$@"
-  else
-    socat - "$MPV_SOCKET" <<< "$1"
-  fi
-}
 
 case "$1" in
-    mpv-next)
-      handle_mpv_error "playlist-next"
-        ;;
-    mpv-pause)
-      handle_mpv_error "cycle pause"
-        ;;
-    mpv-prev)
-      handle_mpv_error "playlist-prev"
-        ;;
-    mpv-stop)
-      handle_mpv_error "quit"
-        ;;
-    playerctl-next)
+    next)
       playerctl next
         ;;
-    playerctl-play-pause)
+    play-pause)
       playerctl play-pause
         ;;
-    playerctl-prev)
+    prev)
       playerctl previous
         ;;
-    playerctl-stop)
+    stop)
       playerctl stop
         ;;
+    status)
+      playerctl status
+        ;;
+    metadata)
+      playerctl metadata
+        ;;
+    volume)
+      if [ -z "$2" ]; then
+        playerctl volume
+      else
+        playerctl volume "$2"
+      fi
+        ;;
+    shuffle)
+      playerctl shuffle
+        ;;
     *)
-      echo "Invalid command"
+      echo "Invalid command. Available commands: next, play-pause, prev, stop, status, metadata, volume, shuffle"
         ;;
 esac
