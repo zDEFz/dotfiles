74b64d79d5037e01e5a2c0a2141a3e28db3ea918
bluetail <a-development+gitea@posteo.de>
2025-03-10 03:04:56 +0100

    move

--- mainrig-MS-7D51/.config/sway/scripts/i3blocksreload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-killall -q swaybar   # Ensure no lingering swaybar processes without output
-
-# Parallel execution of all swaymsg commands (no wait)
-swaymsg "exec --no-startup-id swaybar --bar_id=bar-{1..7}" &
