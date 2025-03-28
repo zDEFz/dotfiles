501db68cec3072f7318a73a7b67bf9583744f6c8
bluetail <a-development+gitea@posteo.de>
2025-03-28 06:00:00 +0100

    Update on Fri Mar 28 06:00:00 AM CET 2025

--- mainrig-MS-7D51/.config/i3blocks/scripts/sway-get-props.sh
+++ mainrig-MS-7D51/.config/i3blocks/scripts/sway-get-props.sh
@@ -1,1 +1,1 @@
-swaymsg -t get_tree | jq -r 'recurse(.nodes[]?; .nodes) | select(.focused == true) | "Type: \(.type), App ID: \(.app_id), Name: \(.name), Shell: \(.shell), Con_ID: \(.id)"'
+swaymsg -t subscribe '["window"]' | jq -r 'select(.change=="focus") | "Type: \(.container.type), App ID: \(.container.app_id), Name: \(.container.name), Shell: \(.container.shell), Con_ID: \(.container.id)"'
