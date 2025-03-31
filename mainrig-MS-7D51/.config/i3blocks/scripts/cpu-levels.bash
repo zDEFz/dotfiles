ff58369a6679fba6422be6924518f2867215e245
bluetail <a-development+gitea@posteo.de>
2025-03-31 21:00:00 +0200

    Update on Mon Mar 31 09:00:00 PM CEST 2025

---
+++ mainrig-MS-7D51/.config/i3blocks/scripts/cpu-levels.bash
@@ -0,0 +1,14 @@
+	
+
+#!/bin/sh
+
+# Define array
+ramp_arr=(▁ ▂ ▃ ▄ ▅ ▆ ▇ █)
+
+sar -P ALL 1 1 | grep -E 'Average:\s+[0-9]+' | while read -r line ; do
+    val=$(echo $line | awk '{cpu_usage=$3} END {printf "%0.2f", cpu_usage}')
+    idx=`echo "scale=4; $val/14.3" | bc`
+    intidx=$( printf "%.0f" $idx )
+    printf "${ramp_arr[$intidx]}"
+done
+echo
