498bed2c0ab2d2f36f652dcb31490f28edbcb0a5
bluetail <a-development+gitea@posteo.de>
2025-04-05 04:00:00 +0200

    Update on Sat Apr  5 04:00:00 AM CEST 2025

--- mainrig-MS-7D51/.config/i3blocks/scripts/sway-get-props.sh
+++ mainrig-MS-7D51/.config/i3blocks/scripts/sway-get-props.sh
@@ -1,1 +1,9 @@
-swaymsg -t subscribe '["window"]' | jq -r 'select(.change=="focus") | "Type: \(.container.type), App ID: \(.container.app_id), Name: \(.container.name), Shell: \(.container.shell), Con_ID: \(.container.id)"'
+#!/bin/sh
+# Subscribe to window focus events and process with jq more efficiently
+swaymsg -t subscribe '["window"]' | 
+  jq --unbuffered -r '
+    # Process only focus events to reduce computation
+    select(.change == "focus") | 
+    # Build string directly instead of concatenating
+    "Type: \(.container.type // ""), App ID: \(.container.app_id // ""), Name: \(.container.name // ""), Shell: \(.container.shell // ""), Con_ID: \(.container.id // "")"
+  '
