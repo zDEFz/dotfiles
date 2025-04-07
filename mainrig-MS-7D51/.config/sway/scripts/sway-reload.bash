fe849cc8c7c0fe7daabb76b9616a9c07e24a5d5c
bluetail <a-development+gitea@posteo.de>
2025-04-07 03:00:00 +0200

    Update on Mon Apr  7 03:00:00 AM CEST 2025

--- mainrig-MS-7D51/.config/sway/scripts/sway-reload.bash
+++ mainrig-MS-7D51/.config/sway/scripts/sway-reload.bash
@@ -2,20 +2,3 @@
 
 # Reload Sway
 swaymsg reload
-
-# Busy-wait until Sway is responsive
-while ! swaymsg -t get_tree >/dev/null 2>&1; do :; done
-
-# Kill lingering swaybar processes
-killall -q swaybar
-
-# Busy-wait until all swaybar processes have stopped
-while pgrep -x swaybar > /dev/null; do :; done
-
-# Start all 7 swaybars in parallel
-for i in {1..3}; do
-  swaymsg "exec --no-startup-id swaybar --bar_id=bar-$i" &
-done
-
-# Play audio in the background
-pw-play '/home/blu/audio/FX/Pokemon/156 - quilava.mp3' &
