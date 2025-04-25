c685bdd658dfb34edd640957728df6cd42bbfbe3
bluetail <a-development+gitea@posteo.de>
2025-04-25 15:00:00 +0200

    Update on Fri Apr 25 03:00:00 PM CEST 2025

--- mainrig-MS-7D51/.config/i3blocks/scripts/sway-get-props.sh
+++ mainrig-MS-7D51/.config/i3blocks/scripts/sway-get-props.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
+
 swaymsg -t subscribe '["window"]' |
   jq --unbuffered -r '
-    # 1) Only care about focus events on containers with a valid id
+    # Only focus events on containers with a valid id
     select(.change == "focus" and .container != null and .container.id != null) |
 
-    # 2) Build an array of “Label: Value” pairs, skipping any empty ones
+    . as $e |
+
+    # Determine shell type: Wayland or XWayland
+    (
+      if $e.container.app_id then "Shell: Wayland"
+      elif $e.container.window_properties then "Shell: XWayland"
+      else "Shell: Unknown"
+      end
+    ) as $shell_type |
+
+    # Build array of label-value strings
     [
-      ("Type: \(.container.type // empty)"),
-      ("App ID: \(.container.app_id // empty)"),
-      ("Name: \(.container.name // empty)"),
-      ("Con_ID: \(.container.id)")
+      $shell_type,
+      ("Type: \($e.container.type // empty)"),
+      ("App ID: \($e.container.app_id // empty)"),
+      ("Name: \($e.container.name // empty)"),
+      ("Con_ID: \($e.container.id)")
     ]
     | map(select(length > 0))
-    # 3) Join into a single comma‑separated string
     | join(", ")
   '
