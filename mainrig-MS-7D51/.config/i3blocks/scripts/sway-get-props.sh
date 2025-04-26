7897836a245b83ade6bf01694a76b852dd0debc2
bluetail <a-development+gitea@posteo.de>
2025-04-26 21:00:00 +0200

    Update on Sat Apr 26 09:00:00 PM CEST 2025

--- mainrig-MS-7D51/.config/i3blocks/scripts/sway-get-props.sh
+++ mainrig-MS-7D51/.config/i3blocks/scripts/sway-get-props.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
 
-swaymsg -t subscribe '["window"]' |
-  jq --unbuffered -r '
-    # Only focus events on containers with a valid id
-    select(.change == "focus" and .container != null and .container.id != null) |
+# Continuously listen for window focus events using swaymsg and process them with jq
+while true; do
+  swaymsg -t subscribe '["window"]' |
+    jq --unbuffered -r '
+      # Only focus events on containers with a valid id
+      select(.change == "focus" and .container != null and .container.id != null) |
 
-    . as $e |
+      . as $e |
 
-    # Determine shell type: Wayland or XWayland
-    (
-      if $e.container.app_id then "Shell: Wayland"
-      elif $e.container.window_properties then "Shell: XWayland"
-      else "Shell: Unknown"
-      end
-    ) as $shell_type |
+      # Determine shell type: Wayland or XWayland
+      (
+        if $e.container.app_id then "Shell: Wayland"
+        elif $e.container.window_properties then "Shell: XWayland"
+        else "Shell: Unknown"
+        end
+      ) as $shell_type |
 
-    # Build array of label-value strings
-    [
-      $shell_type,
-      ("Type: \($e.container.type // empty)"),
-      ("App ID: \($e.container.app_id // empty)"),
-      ("Name: \($e.container.name // empty)"),
-      ("Con_ID: \($e.container.id)")
-    ]
-    | map(select(length > 0))
-    | join(", ")
-  '
+      # Build array of label-value strings
+      [
+        $shell_type,
+        ("Type: \($e.container.type // empty)"),
+        ("App ID: \($e.container.app_id // empty)"),
+        ("Name: \($e.container.name // empty)"),
+        ("Con_ID: \($e.container.id)")
+      ]
+      | map(select(length > 0))
+      | join(", ")
+    '
+done
