0aabb1b71981b890462d3836808a9cd1b5ce6852
bluetail <a-development+gitea@posteo.de>
2025-04-27 15:00:00 +0200

    Update on Sun Apr 27 03:00:00 PM CEST 2025

--- mainrig-MS-7D51/.config/mpv/profiles/openmusic/scripts/progressbar.lua
+++ mainrig-MS-7D51/.config/mpv/profiles/openmusic/scripts/progressbar.lua
@@ -276,7 +276,7 @@ the binding `f cycle pause; script-binding progressbar/toggle-inactive-bar`, it
 is possible to have the bar be persistently present only in windowed or
 fullscreen contexts, depending on the default setting.
 ]]
-settings['bar-height-inactive'] = 0
+settings['bar-height-inactive'] = 3
 helpText['bar-height-inactive'] = [[Sets the height of the bar display when the mouse is not in the active zone and
 there is no request-display active. A value of 0 or less will cause bar-hide-
 inactive to be set to true and the bar height to be set to 1. This should result
@@ -384,7 +384,7 @@ settings['remaining-bottom-margin'] = 0
 helpText['remaining-bottom-margin'] = [[Controls how far above the expanded progress bar the remaining time display is
 positioned.
 ]]
-settings['enable-hover-time'] = true
+settings['enable-hover-time'] = false
 helpText['enable-hover-time'] = [[Sets whether or not the calculated time corresponding to the mouse position
 is displayed when the mouse hovers over the progress bar.
 ]]
