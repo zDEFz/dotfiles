5c449241db43b04e68e27cf094d678013169589c
bluetail <a-development+gitea@posteo.de>
2025-04-27 21:00:00 +0200

    Update on Sun Apr 27 09:00:00 PM CEST 2025

--- mainrig-MS-7D51/.config/mpv/profiles/openmusic/scripts/filename_remaining_files.lua
+++ mainrig-MS-7D51/.config/mpv/profiles/openmusic/scripts/filename_remaining_files.lua
@@ -45,35 +45,43 @@ local function wrap_text(text, max_width)
 end
 
 -- Update top overlay with folder and filename on separate lines (with filename wrapping)
+-- Helper: remove file extension
+local function remove_extension(filename)
+    return filename:match("^(.*)%.") or filename
+end
+
+-- Update top overlay with folder and filename on separate lines (with filename wrapping)
 local function update_file_info()
     if not show_file_info then
         top_overlay:remove()
         return
     end
-    
+
     local full = mp.get_property("path")
     local name = mp.get_property("filename")
     if not (full and name) then
         top_overlay:remove()
         return
     end
-    
+
     local folder = extract_folder_name(full)
-    
+    local name_no_ext = remove_extension(name)
+
     -- Wrap filename if needed
-    local wrapped_filename = wrap_text(name, max_width_chars)
-    
+    local wrapped_filename = wrap_text(name_no_ext, max_width_chars)
+
     -- Create ASS markup with folder and wrapped filename
     local ass_text = string.format(
-        "{\\an7\\bord3\\shad0\\fs%d\\b1\\q2\\3c&H%s&\\3a&H00&}{\\1c&H%s&}%s\\N{\\1c&H%s&}%s", 
-        font_size, bg_color, 
+        "{\\an4\\bord3\\shad0\\fs%d\\b1\\q2\\3c&H%s&\\3a&H00&}{\\1c&H%s&}%s\\N{\\1c&H%s&}%s", 
+        font_size, bg_color,
         folder_color, folder,
         filename_color, wrapped_filename
     )
-    
+
     top_overlay.data = ass_text
     top_overlay:update()
 end
+
 
 -- Update bottom overlay (Remaining: X/Y)
 local function update_remaining()
@@ -87,6 +95,16 @@ local function update_remaining()
     
     -- Apply a black background box with counter color text (bottom center)
     local ass_text = string.format(
+
+-- Top-left	     \an7
+-- Top-center	 \an8
+-- Top-right	 \an9
+-- Middle-left	 \an4
+-- Middle-center \an5
+-- Middle-right	 \an6
+-- Bottom-left	 \an1
+-- Bottom-center \an2
+-- Bottom-right	 \an3 
         "{\\an2\\bord3\\shad0\\fs%d\\b1\\q2\\1c&H%s&\\3c&H%s&\\3a&H00&}Remaining: %d/%d", 
         font_size, counter_color, bg_color, pos, tot
     )
