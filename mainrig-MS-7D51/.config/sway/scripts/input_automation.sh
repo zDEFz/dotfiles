9528f08669d38c8d6555f716d6cff8be6667884a
bluetail <a-development+bluetail@posteo.de>
2025-06-01 04:00:00 +0200

    Update on Sun Jun  1 04:00:00 AM CEST 2025

--- mainrig-MS-7D51/.config/sway/scripts/input_automation.sh
+++ mainrig-MS-7D51/.config/sway/scripts/input_automation.sh
@@ -4,27 +4,26 @@
 
 CHOICE=$(echo -e "Type clipboard\nType vmpwd\nType date\nCancel" | wofi --dmenu --hide-scroll -Dlayer=overlay)
 
-DEBOUNCE_CTRL() { echo "key leftctrl" | dotoolc;  } 
+RELEASE_CTRL() { echo "key leftctrl" | dotoolc;  } 
 # Exit immediately if ESC or nothing selected
 [ -z "$CHOICE" ] && exit 0
 
 case "$CHOICE" in
     "Type clipboard")
-        {
-          DEBOUNCE_CTRL
-          sleep 0.25
-          echo -n type wl-paste
-        }  | dotoolc
+       { 
+          RELEASE_CTRL
+          echo type "$(wl-paste)"
+       } | dotoolc
         ;;
     "Type vmpwd")
        { 
-          DEBOUNCE_CTRL
+          RELEASE_CTRL
           echo "type W50zyOgGmdY9DZbuvRHy";  
        }  | dotoolc       
         ;;
     "Type date")
     {
-      DEBOUNCE_CTRL
+      RELEASE_CTRL
       echo "type $(date --iso-8601)"
     } | dotoolc
         ;;
