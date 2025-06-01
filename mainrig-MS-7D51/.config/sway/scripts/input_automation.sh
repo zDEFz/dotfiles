daa91cbd9af26c83011b1e726d339b46e5fe22a1
bluetail <a-development+bluetail@posteo.de>
2025-06-01 03:00:00 +0200

    Update on Sun Jun  1 03:00:00 AM CEST 2025

--- mainrig-MS-7D51/.config/sway/scripts/input_automation.sh
+++ mainrig-MS-7D51/.config/sway/scripts/input_automation.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-[ -z "$(pgrep -x dotoold)" ] && { dotoold; } # start dotoold if not running
+# If 'dotoold' is not running, start it in the background with nohup (to ignore hangups) and discard all output
+! pgrep -x dotoold > /dev/null && nohup dotoold > /dev/null 2>&1 &
 
 CHOICE=$(echo -e "Type clipboard\nType vmpwd\nType date\nCancel" | wofi --dmenu --hide-scroll -Dlayer=overlay)
 
+DEBOUNCE_CTRL() { echo "key leftctrl" | dotoolc;  } 
 # Exit immediately if ESC or nothing selected
 [ -z "$CHOICE" ] && exit 0
 
 case "$CHOICE" in
     "Type clipboard")
-        echo "type $(wl-paste)" | dotoolc
+        {
+          DEBOUNCE_CTRL
+          sleep 0.25
+          echo -n type wl-paste
+        }  | dotoolc
         ;;
     "Type vmpwd")
-        echo "type W50zyOgGmdY9DZbuvRHy" | dotoolc
+       { 
+          DEBOUNCE_CTRL
+          echo "type W50zyOgGmdY9DZbuvRHy";  
+       }  | dotoolc       
         ;;
     "Type date")
-        echo "type $(date --iso-8601)" | dotoolc
+    {
+      DEBOUNCE_CTRL
+      echo "type $(date --iso-8601)"
+    } | dotoolc
         ;;
     *)
         notify-send "No valid selection"
