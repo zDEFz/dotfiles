6c005272c00774d5a608a55f030535c74a3a4c38
bluetail <a-development+bluetail@posteo.de>
2025-12-31 15:58:20 +0100

    gitignore upd

--- mainrig-MS-7E49/.config/sway/appspawn.sh
+++
@@ -1,7 +0,0 @@
-[ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] && exit
-
-if swaymsg "[$1=\"$2\"] scratchpad show"; then
-   swaymsg "[$1=\"$2\"] move position center"
-else
-  swaymsg "exec --no-startup-id $(echo "$@" | cut -d ' ' -f3-)"
-fi
