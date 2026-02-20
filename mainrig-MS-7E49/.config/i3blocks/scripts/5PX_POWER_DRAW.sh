dd35f85b1ff27a1cfe086806e4fc3d542a02c677
bluetail <a-development+bluetail@posteo.de>
2026-02-20 18:00:32 +0100

    process and synchronize 5PX_POWER_DRAW.sh contents

--- mainrig-MS-7E49/.config/i3blocks/scripts/5PX_POWER_DRAW.sh
+++ mainrig-MS-7E49/.config/i3blocks/scripts/5PX_POWER_DRAW.sh
@@ -5,21 +5,28 @@ HOST="eaton-5PX-2200i"
 COMMUNITY="public"
 MIB_PATH="/usr/share/snmp/mibs:$HOME/MIBs"
 
-# 1. Fetch only the reliable data points
-mapfile -t data < <(snmpget -v2c -c "$COMMUNITY" -M "$MIB_PATH" -m XUPS-MIB -Oqvn "$HOST" \
-    XUPS-MIB::xupsOutputTotalWatts.0 \
-    XUPS-MIB::xupsBatTimeRemaining.0 \
-    XUPS-MIB::xupsBatCapacity.0 \
-    2>/dev/null | tr -dc '0-9\n')
+# List your OIDs here in the order you want them fetched
+OIDS=(
+    "XUPS-MIB::xupsOutputPercentLoad.1"
+    "XUPS-MIB::xupsOutputTotalWatts.0"
+    "XUPS-MIB::xupsBatTimeRemaining.0"
+    "XUPS-MIB::xupsBatCapacity.0"
+)
 
-# 2. Assign variables
-out_w=${data[0]:-0}
-secs=${data[1]:-0}
-chg=${data[2]:-0}
+# 1. Fetch data into a temporary array
+mapfile -t data < <(snmpget -v2c -c "$COMMUNITY" -M "$MIB_PATH" -m XUPS-MIB -Oqvn "$HOST" "${OIDS[@]}" 2>/dev/null | tr -cd '0-9\n')
 
-# 3. Format Time
-hrs=$(( secs / 3600 ))
-min=$(( (secs % 3600) / 60 ))
+# 2. Explictly name them for readability
+ld=${data[0]:-0}
+wt=${data[1]:-0}
+tr=${data[2]:-0}
+bc=${data[3]:-0}
 
-# 4. Final Output (Efficiency removed per your request)
-echo "${out_w}W | ${hrs}h ${min}m | ${chg}% chg"
+# 3. Format time (1h30m)
+time_fmt="$((tr/3600))h$(((tr%3600)/60))m"
+
+# 4. Final Output
+echo ">>> ${ld}% ${wt}W | bat ${bc}% ${time_fmt}"
+
+
+
