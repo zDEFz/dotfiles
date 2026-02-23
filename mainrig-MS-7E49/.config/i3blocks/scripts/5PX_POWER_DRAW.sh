4ef9d4038a15661ff06acb6520e5662987930b01
bluetail <a-development+bluetail@posteo.de>
2026-02-23 08:57:38 +0100

    Add cost calculations to power draw script

--- mainrig-MS-7E49/.config/i3blocks/scripts/5PX_POWER_DRAW.sh
+++ mainrig-MS-7E49/.config/i3blocks/scripts/5PX_POWER_DRAW.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
-# 5PX_POWER_DRAW.sh - Fetches power draw and battery status from Eaton 5PX UPS via SNMP
+# 5PX_POWER_DRAW.sh - Optimized for i3blocks
 
 # Configuration
 HOST="eaton-5PX-2200i"
 COMMUNITY="public"
 MIB_PATH="/usr/share/snmp/mibs:$HOME/MIBs"
+KWH_PRICE=0.35
 
-# List your OIDs here in the order you want them fetched
-OIDS=(
-    "XUPS-MIB::xupsOutputPercentLoad.1"
-    "XUPS-MIB::xupsOutputTotalWatts.0"
-    "XUPS-MIB::xupsBatTimeRemaining.0"
-    "XUPS-MIB::xupsBatCapacity.0"
-)
+# 1. Fetch data
+mapfile -t data < <(snmpget -v2c -c "$COMMUNITY" -M "$MIB_PATH" -m XUPS-MIB -Oqvn "$HOST" \
+    XUPS-MIB::xupsOutputPercentLoad.1 \
+    XUPS-MIB::xupsOutputTotalWatts.0 \
+    XUPS-MIB::xupsBatTimeRemaining.0 \
+    XUPS-MIB::xupsBatCapacity.0 2>/dev/null | tr -cd '0-9\n')
 
-# 1. Fetch data into a temporary array
-mapfile -t data < <(snmpget -v2c -c "$COMMUNITY" -M "$MIB_PATH" -m XUPS-MIB -Oqvn "$HOST" "${OIDS[@]}" 2>/dev/null | tr -cd '0-9\n')
-
-# 2. Explictly name them for readability
 ld=${data[0]:-0}
 wt=${data[1]:-0}
 tr=${data[2]:-0}
 bc=${data[3]:-0}
 
-# 3. Format time (1h30m)
+# 2. Calculations
 time_fmt="$((tr/3600))h$(((tr%3600)/60))m"
 
-# 4. Final Output
-echo ">>> ${ld}% ${wt}W | bat ${bc}% ${time_fmt}"
+# Monthly cost (730 hours)
+m_cost=$(echo "scale=2; ($wt * 730 / 1000) * $KWH_PRICE" | bc)
+# Daily cost (24 hours)
+d_cost=$(echo "scale=2; ($wt * 24 / 1000) * $KWH_PRICE" | bc)
 
+# 3. i3blocks Output
+# Line 1: Full text (Visible on the bar)
+echo ">>> ${ld}% ${wt}W | bat ${bc}% ${time_fmt} | €${d_cost}/d | €${m_cost}/mo"
 
+# Line 2: Short text (Only shows if space is very tight)
+echo "${wt}W | €${m_cost}/mo"
 
+# Line 3: Color
+if [ "$bc" -lt 50 ]; then
+    echo "#FF0000"
+fi
