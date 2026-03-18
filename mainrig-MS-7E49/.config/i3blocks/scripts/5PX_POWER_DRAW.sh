10ce7e6d18c027275e3d8e0d39105d9d81e4ec7d
bluetail <a-development+bluetail@posteo.de>
2026-03-18 07:23:54 +0100

    Simplified power draw script

--- mainrig-MS-7E49/.config/i3blocks/scripts/5PX_POWER_DRAW.sh
+++ mainrig-MS-7E49/.config/i3blocks/scripts/5PX_POWER_DRAW.sh
@@ -8,14 +8,13 @@ MIB_PATH="/usr/share/snmp/mibs:$HOME/MIBs"
 KWH_PRICE_MILLICENTS=350 # 0.35 * 1000 to avoid floats in bash
 
 # 1. Fetch data - Using -Oqv to get values only
-# We read into a variable first to avoid multiple forks
 raw_data=$(snmpget -v2c -c "$COMMUNITY" -M "$MIB_PATH" -m XUPS-MIB -Oqv "$HOST" \
     XUPS-MIB::xupsOutputPercentLoad.1 \
     XUPS-MIB::xupsOutputTotalWatts.0 \
     XUPS-MIB::xupsBatTimeRemaining.0 \
     XUPS-MIB::xupsBatCapacity.0 2>/dev/null)
 
-# Convert multiline string to array without calling 'tr'
+# Convert multiline string to array
 data="($raw_data)"
 
 ld=${data[0]:-0}
@@ -24,22 +23,15 @@ tr=${data[2]:-0}
 bc=${data[3]:-0}
 
 # 2. Native Bash Calculations
-time_h=$((tr / 3600))
+time_h=$(( tr / 3600))
 time_m=$(( (tr % 3600) / 60 ))
 
-# Calculate costs in "millicents" then format with printf
-# Daily: (Watts * 24 hours * Price) / 1000
+# Calculate costs
 d_total_mc=$(( wt * 24 * KWH_PRICE_MILLICENTS / 1000 ))
 m_total_mc=$(( wt * 730 * KWH_PRICE_MILLICENTS / 1000 ))
 
-# Format as currency (converts e.g., 350 to 0.35)
 d_cost=$(printf "%d.%02d" $((d_total_mc/100)) $((d_total_mc%100)))
 m_cost=$(printf "%d.%02d" $((m_total_mc/100)) $((m_total_mc%100)))
 
-# 3. i3blocks Output
-echo ">>> ${ld}% ${wt}W | bat ${bc}% ${time_h}h${time_m}m | EUR ${d_cost}/d | EUR ${m_cost}/mo"
-echo "${wt}W | EUR ${m_cost}/mo"
-
-if [ "$bc" -lt 50 ]; then
-    echo "#FF0000"
-fi
+# 3. Single Line i3blocks Output
+echo "${ld}% ${wt}W | bat ${bc}% ${time_h}h${time_m}m | EUR ${d_cost}/d | EUR ${m_cost}/mo"
