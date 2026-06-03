2d983661799566a67819ba4f0240cf53c504e83f
bluetail <a-development+bluetail@posteo.de>
2026-06-04 01:36:56 +0200

    Add new script for 5PX power draw monitoring

---
+++ mainrig-MS-7E49/.config/i3blocks/scripts/old/5PX_POWER_DRAW.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# 5PX_POWER_DRAW.sh - Optimized for i3blocks
+
+# Configuration
+HOST="eaton-5PX-2200i"
+COMMUNITY="public"
+MIB_PATH="/usr/share/snmp/mibs:$HOME/MIBs"
+KWH_PRICE_MILLICENTS=350 # 0.35 * 1000 to avoid floats in bash
+
+# 1. Fetch data - Using -Oqv to get values only
+raw_data=$(snmpget -v2c -c "$COMMUNITY" -M "$MIB_PATH" -m XUPS-MIB -Oqv "$HOST" \
+    XUPS-MIB::xupsOutputPercentLoad.1 \
+    XUPS-MIB::xupsOutputTotalWatts.0 \
+    XUPS-MIB::xupsBatTimeRemaining.0 \
+    XUPS-MIB::xupsBatCapacity.0 2>/dev/null)
+
+# Convert multiline string to array
+data="($raw_data)"
+
+ld=${data[0]:-0}
+wt=${data[1]:-0}
+tr=${data[2]:-0}
+bc=${data[3]:-0}
+
+# 2. Native Bash Calculations
+time_h=$(( tr / 3600))
+time_m=$(( (tr % 3600) / 60 ))
+
+# Calculate costs
+d_total_mc=$(( wt * 24 * KWH_PRICE_MILLICENTS / 1000 ))
+m_total_mc=$(( wt * 730 * KWH_PRICE_MILLICENTS / 1000 ))
+
+d_cost=$(printf "%d.%02d" $((d_total_mc/100)) $((d_total_mc%100)))
+m_cost=$(printf "%d.%02d" $((m_total_mc/100)) $((m_total_mc%100)))
+
+# 3. Single Line i3blocks Output
+# We use 'tr -d "\n"' to ensure no internal variables break the line, 
+# and 'xargs' to clean up any leading/trailing whitespace.
+echo "${ld}% ${wt}W | bat ${bc}% ${time_h}h${time_m}m | EUR ${d_cost}/d | EUR ${m_cost}/mo" | tr -d '\n' | xargs
